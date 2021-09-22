6f94ed3a6f8af313a77df2e2be35f4b788ca0587
s6135410012 <s6135410012@sau.ac.th>
2021-09-22 15:06:38 +0700

    Code V.3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_bc01/views/login_ui.dart';
 
-main() {
+void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
